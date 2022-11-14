525600e1007b5062d9e66cd9e56c600be8761c19
Jason Ling <Jasonling1996@gmail.com>
2022-11-14 21:24:29 +0000

    Added players class with name, lives and streak

---
+++ players.rb
@@ -0,0 +1,17 @@
+class Players
+  attr_accessor :lives, :name
+
+  def initialize(name, lives = 3)
+    @name = name
+    @lives = lives
+  end
+
+  def alive?
+    @lives > 0
+  end
+
+  def lose_health
+    @lives -= 1
+  end
+
+end
